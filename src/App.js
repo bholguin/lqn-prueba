14bb12e9af06193f0fa9a6bc80d47d4e939674d6
Brayhan <54687783+bholguin@users.noreply.github.com>
2022-01-25 12:13:43 -0500

    upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { Card } from "@mui/material";
+
+function App() {
+  const StyledBody = styled.div`
+    ${tw`bg-blue-400 h-[100vh] text-align[center]`}
+  `;
+
+  return (
+    <StyledBody>
+      <header>
+        <Card>
+          <img src={logo} className="App-logo" alt="logo" />
+        </Card>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </StyledBody>
+  );
+}
+
+export default App;
